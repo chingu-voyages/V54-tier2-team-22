99889e897afd53f34527613ef9dda8b0d127a329
shaimaa <shisko939@gmail.com>
2025-03-13 07:14:52 +0200

    feat(app): add Header component and ThemeProvider

--- ai-helper/src/App.jsx
+++ ai-helper/src/App.jsx
@@ -1,14 +1,13 @@
-
-import './App.css'
+import './App.css';
+import Header from './components/header';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <ThemeProvider>
+      <Header />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
