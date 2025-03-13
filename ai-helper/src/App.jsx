a7c7090257b7a7f5292f7d0be1797fc2c28c53e9
shaimaa <shisko939@gmail.com>
2025-03-13 08:35:41 +0200

    add heading

--- ai-helper/src/App.jsx
+++ ai-helper/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <ThemeProvider>
       <Header />
+      <h1 className="dark:text-Neutral-100 text-Neutral-900 font-bold text-[64px] max-sm:text-[40px] tracking-[-1px] leading-[100%] max-w-[510px] max-sm:max-w-[343px] text-center">
+      Get High-Quality AI Answers
+      </h1>
     </ThemeProvider>
   );
 }
