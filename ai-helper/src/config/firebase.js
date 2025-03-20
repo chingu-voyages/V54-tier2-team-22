51bd8f3298704dfe2ded6a67f4ccd590fc4487c1
semilore <semilore@omotade-michaels.com>
2025-03-20 07:49:49 +0100

    feat: firebase config

---
+++ ai-helper/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA28pCNIYOfrlGGEv1HQZPOvW95iX2-i2Q",
+    authDomain: "v54-team22.firebaseapp.com",
+    databaseURL: "https://v54-team22-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "v54-team22",
+    storageBucket: "v54-team22.firebasestorage.app",
+    messagingSenderId: "160703570201",
+    appId: "1:160703570201:web:4d854cde9d006edb4d6588"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
