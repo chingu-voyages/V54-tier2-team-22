779f3bf7d468e0f54fd8764c115a1480822389fc
shaimaa <shisko939@gmail.com>
2025-03-26 15:30:34 +0200

    feat: make Header global and update Signupform

--- ai-helper/src/views/Sign-in.jsx
+++ ai-helper/src/views/Sign-in.jsx
@@ -1,18 +1,14 @@
-import Signinform from "../components/signinForm";
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer';
+import Signinform from '../components/signinForm';
 import { ThemeProvider } from '../context/ThemeContext';
 
 export default function Signin() {
-    return (
-        <ThemeProvider>
-            <div className="w-screen h-screen flex flex-col justify-center items-center">
-                <section className=" md:w-6/12 h-full w-10/12 flex flex-col justify-center items-center">
-                    <Header />
-                    <Signinform />
-                </section>
-            </div>
-            {/* <Footer /> */}
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <section className=" md:w-6/12 h-full w-10/12 flex flex-col justify-center items-center">
+          <Signinform />
+        </section>
+      </div>
+    </ThemeProvider>
+  );
 }
--- ai-helper/src/views/Sign-up.jsx
+++ ai-helper/src/views/Sign-up.jsx
@@ -1,16 +1,11 @@
-import Signupform from "../components/signupForm";
-import Header from '../components/Header/Header';
-import { ThemeProvider } from '../context/ThemeContext';
+import Signupform from '../components/signupForm';
 
 export default function Signin() {
-    return (
-        <ThemeProvider>
-            <div className="w-screen h-screen flex flex-col justify-center items-center">
-                <section className=" md:w-6/12 h-full w-10/12 flex flex-col justify-center items-center">
-                    <Header />
-                    <Signupform />
-                </section>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <main className="flex flex-col w-full items-center gap-[40px] max-xl:gap-[40px] pt-[32px] max-xl:pt-[16px] px-[225px] max-xl:px-[32px] max-sm:px-[16px]">
+      <section className="  h-full w-full lg:w-3/4 flex flex-col justify-center items-center">
+        <Signupform />
+      </section>
+    </main>
+  );
 }
