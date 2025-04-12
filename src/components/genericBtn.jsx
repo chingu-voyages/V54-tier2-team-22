2513add66341be9714bf4f07d4b3ee7b00c598ea
shaimaa <shisko939@gmail.com>
2025-04-12 13:40:06 +0200

    fix: move Google icon to public folder to ensure it works in deployed build

--- src/components/genericBtn.jsx
+++ src/components/genericBtn.jsx
@@ -1,7 +1,7 @@
 export default function GenericBtn({ text, type }) {
     return (
         <>
-            <button type={text} className="bg-[#bf7df1] p-2 text-white text-lg rounded-lg mx-1">{text}</button>
+            <button type={text} className="bg-[#bf7df1] p-2 mt-3 text-white text-lg rounded-lg mx-1">{text}</button>
         </>
     )
 }
--- src/components/signinForm.jsx
+++ src/components/signinForm.jsx
@@ -1,133 +1,137 @@
-import GenericBtn from "./genericBtn";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { auth, googleProvider, githubProvider } from "../config/firebase";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import GenericBtn from './genericBtn';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { auth, googleProvider, githubProvider } from '../config/firebase';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import GitHub from "../icons/GitHub";
-
+import GitHub from '../icons/GitHub';
+import google from '/assets/images/icons8-google.svg';
 
 export default function Signinform() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const handleSignInWithEmailAndPassword = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // alert('Signed in successfully');
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleSignInWithEmailAndPassword = async (e) => {
-        e.preventDefault();
-        try {
-            await signInWithEmailAndPassword(auth, email, password);
-            // alert('Signed in successfully');
-            navigate('/', { replace: true });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      // console.log(result.user);
+      // alert("Signed in successfully");
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.error('Error signing in with Google:', error.message);
+    }
+  };
 
-    const signInWithGoogle = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            // console.log(result.user); 
-            // alert("Signed in successfully");
-            navigate('/', { replace: true });
-        } catch (error) {
-            console.error("Error signing in with Google:", error.message);
-        }
-    };
+  const signInWithGithub = async () => {
+    try {
+      const result = await signInWithPopup(auth, githubProvider);
+      // console.log(result.user);
+      // alert("Signed in successfully");
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.error('Error signing in with Github:', error.message);
+    }
+  };
 
-    const signInWithGithub = async () => {
-        try {
-            const result = await signInWithPopup(auth, githubProvider);
-            // console.log(result.user); 
-            // alert("Signed in successfully");
-            navigate('/', { replace: true });
-        } catch (error) {
-            console.error("Error signing in with Github:", error.message);
-        }
-    };
+  return (
+    // <div className="mt-[5%] flex flex-col justify-start p-12 md:pl-5 md:pt-14 md:pb-4 md:pr-28 gap-4">
+    <div className="flex md:w-[65%] w-[85%] h-full flex-col justify-center">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-4xl dark:text-Neutral-100 text-Neutral-900 mx-auto">
+          Welcome Back
+        </h1>
 
-    return (
-        // <div className="mt-[5%] flex flex-col justify-start p-12 md:pl-5 md:pt-14 md:pb-4 md:pr-28 gap-4">
-        <div className="flex md:w-[65%] w-[85%] h-full flex-col justify-center">
-
-            <div className="flex flex-col gap-5">
-                <h1 className="text-4xl dark:text-Neutral-100 text-Neutral-900 mx-auto">Welcome Back</h1>
-
-                <div className="">
-                    <button
-                        onClick={signInWithGoogle}
-                        className="flex justify-center gap-2 border dark:border-Neutral-100 border-Neutral-900 px-3 py-2 rounded-xl w-full cursor-pointer"
-                        target="_blank"
-                    >
-                        <img src="../../../src/assets/icons8-google.svg" alt="" />
-                        <p>Sign in with Google</p>
-                    </button>
-
-                </div>
-                <div className="">
-                    <button
-                        onClick={signInWithGithub}
-                        className="flex justify-center gap-2 border dark:border-Neutral-100 border-Neutral-900 px-3 py-2 rounded-xl w-full"
-                        target="_blank"
-                    >
-                        <GitHub />
-                        <p>Sign in with Github</p>
-                    </button>
-                </div>
-
-                <div className="flex justify-center dark:text-Neutral-100 text-Neutral-900 text-2xl">
-                    <p>OR</p>
-                </div>
-
-                <form onSubmit={handleSignInWithEmailAndPassword} action="" className="flex flex-col gap-5">
-
-
-                    <div className="flex flex-col gap-3">
-                        <label className="text-2xl dark:text-Neutral-100 text-Neutral-900" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="border border-[#878787] p-2 rounded-xl bg-[#EDEDED] dark:text-Neutral-900 text-Neutral-100"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter your email"
-                            id="email"
-                            required
-                        />
-                    </div>
-
-                    <div className="flex flex-col gap-3">
-                        <label className="text-2xl dark:text-Neutral-100 text-Neutral-900" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="border border-[#878787] p-2 rounded-xl bg-[#EDEDED] dark:text-Neutral-900 text-Neutral-100"
-                            name="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Enter your password"
-                            id="password"
-                            required
-                        />
-                    </div>
-
-                    <GenericBtn text={"Sign in"} type={"submit"} />
-                </form>
-                <div className="flex justify-center dark:text-Neutral-100 text-Neutral-900">
-                    <Link to="/signup" className="cursor-pointer">
-                        <p className="font-semibold">
-                            Don't have an account?{" "}
-                            <span className="text-[#bf7df1] font-semibold">
-                                {"   "}
-                                Sign up
-                            </span>
-                        </p>
-                    </Link>
-                </div>
-            </div>
+        <div className="">
+          <button
+            onClick={signInWithGoogle}
+            className="flex justify-center gap-2 border dark:border-Neutral-100 border-Neutral-900 px-3 py-2 rounded-xl w-full cursor-pointer"
+            target="_blank"
+          >
+            <img src={google} alt="google icon" />
+            <p>Sign in with Google</p>
+          </button>
         </div>
-    );
+        <div className="">
+          <button
+            onClick={signInWithGithub}
+            className="flex justify-center gap-2 border dark:border-Neutral-100 border-Neutral-900 px-3 py-2 rounded-xl w-full"
+            target="_blank"
+          >
+            <GitHub />
+            <p>Sign in with Github</p>
+          </button>
+        </div>
+
+        <div className="flex justify-center dark:text-Neutral-100 text-Neutral-900 text-2xl">
+          <p>OR</p>
+        </div>
+
+        <form
+          onSubmit={handleSignInWithEmailAndPassword}
+          action=""
+          className="flex flex-col gap-5"
+        >
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-2xl dark:text-Neutral-100 text-Neutral-900"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="border border-[#878787] p-2 rounded-xl bg-[#EDEDED] dark:text-Neutral-900 text-Neutral-100"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              id="email"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-2xl dark:text-Neutral-100 text-Neutral-900"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="border border-[#878787] p-2 rounded-xl bg-[#EDEDED] dark:text-Neutral-900 text-Neutral-100"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              id="password"
+              required
+            />
+          </div>
+
+          <GenericBtn text={'Sign in'} type={'submit'} />
+        </form>
+        <div className="flex justify-center dark:text-Neutral-100 text-Neutral-900">
+          <Link to="/signup" className="cursor-pointer">
+            <p className="font-semibold">
+              Don't have an account?{' '}
+              <span className="text-[#bf7df1] font-semibold">Sign up</span>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
